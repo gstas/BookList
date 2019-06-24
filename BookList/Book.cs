81c408ac39dffa3158cb7eae74d85be4cf1dd4e6
user <gstas@gmail.com>
2019-06-24 20:17:35 +0300

    v2

---
+++ BookList/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BookList
+{
+    class Book
+    {
+        string title;
+       
+
+        public string Title { get => title; set => title = value; }
+   
+     
+        public Book(Book book)
+        {
+            //
+            this.title = book.title;
+          
+        }
+
+        public Book(string title)
+        {
+            Title = title;
+        }
+
+        public bool FindByTitle(string title)
+        {
+            if (this.title.ToLower().Contains(title.ToLower()))
+                return true;
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+                      
+            return Title.ToUpper();
+        }
+    }
+}
---
+++ BookList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Книга1");
+            Book book2 = new Book("Книга2");
+
+            Library lib = new Library();
+            lib.Add(book1);
+            lib.Add(book2);
+
+            Console.WriteLine(lib);
+
+            lib.BookAt(1);
+            Console.WriteLine(lib);
+        }
+    }
+}
